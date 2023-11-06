0c429bd1f4a33313fb2e99e857efdd78e6024d60
Lebamlak <lebamlak@Lebamlaks-MacBook-Pro.local>
2023-11-05 23:40:58 -0800

    added reactstrap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { /* components from reactstrap */ } from 'reactstrap';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    // other Vite config options
+    build: {
+      rollupOptions: {
+        external: ['reactstrap'],
+      },
+    },
 })
